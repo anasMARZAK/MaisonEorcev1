f22a1d82a7737aef624268b369e90a854b3e8e6c
anasMarzak <anasmarzak16@gmail.com>
2026-07-04 19:50:21 +0100

    style: reduce loading screen fadeout duration to 0.8s

--- components/layout/loading-screen.tsx
+++ components/layout/loading-screen.tsx
@@ -43,7 +43,7 @@ export default function LoadingScreen() {
           initial={{ opacity: 1 }}
           exit={{ 
             opacity: 0, 
-            transition: { duration: 2.0, ease: [0.76, 0, 0.24, 1] } 
+            transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] } 
           }}
           className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-[#0F0E0C] text-[#FDFBF7]"
         >
